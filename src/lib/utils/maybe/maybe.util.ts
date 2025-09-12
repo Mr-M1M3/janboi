af19f7f01c6844d456ba6e2139c706f6d69e1a62
Abir Sheikh <abir.sheikh.4044@gmail.com>
2025-09-13 03:55:15 +0600

    renamed result and maybe util

---
+++ src/lib/utils/maybe/maybe.util.ts
@@ -0,0 +1,48 @@
+export default class Maybe<T> {
+  private value: T | null;
+
+  constructor(val: T | null = null) {
+    this.value = val;
+  }
+  //useful methods
+  public is_value(): boolean {
+    if (this.value === null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public is_not_a_value(): boolean {
+    return !this.is_value();
+  }
+  public expect(msg: string): T {
+    if (this.is_value()) {
+      return this.value as T;
+    } else {
+      throw new Error(msg);
+    }
+  }
+  public unwrap(): T {
+    return this.expect('Expected a value but didnt get one.')
+  }
+  public unwrap_or(def: T): T {
+    if (this.is_value()) {
+      return this.value as T;
+    } else {
+      return def;
+    }
+  }
+  public unwrap_or_else(fn: () => T): T {
+    return this.unwrap_or(fn())
+  }
+  public map<U>(fn: (v: T) => U): Maybe<U> {
+    if (this.is_value()) {
+      return new Maybe<U>(fn(this.value as T));
+    } else {
+      return new Maybe<U>(null);
+    }
+  }
+  public transform<To>(fn: (from: typeof this) => To): To {
+    return fn(this);
+  }
+}
---
+++ src/lib/utils/result/result.util.ts
@@ -0,0 +1,75 @@
+import Maybe from "../maybe/maybe.util.js";
+
+export default class Result<T, E> {
+  private ok: Maybe<T>;
+  private err: Maybe<E>;
+
+  private constructor(ok: T | null, err: E | null) {
+    this.ok = new Maybe<T>(ok);
+    this.err = new Maybe<E>(err);
+  }
+
+  //public interface to construct an instance
+  public static Ok<T, E>(val: T): Result<T, E> {
+    return new Result<T, E>(val, null);
+  }
+  public static Err<T, E>(err: E): Result<T, E> {
+    return new Result<T, E>(null, err);
+  }
+  //useful methods
+  public is_ok(): boolean {
+    return this.ok.is_value();
+  }
+  public is_error(): boolean {
+    return this.err.is_value();
+  }
+  public ok_value(): Maybe<T> {
+    return this.ok;
+  }
+  public err_value(): Maybe<E> {
+    return this.err;
+  }
+  public expect(msg: string): T {
+    return this.ok.expect(msg);
+  }
+  public expect_err(msg: string): E {
+    return this.err.expect(msg);
+  }
+  public unwrap(): T {
+    return this.ok.unwrap();
+  }
+  public unwrap_err(): E {
+    return this.err.unwrap();
+  }
+  public unwrap_or(def: T): T {
+    return this.ok.unwrap_or(def);
+  }
+  public unwrap_err_or(def: E): E {
+    return this.err.unwrap_or(def);
+  }
+  public unwrap_or_else(fn: () => T): T {
+    return this.ok.unwrap_or_else(fn);
+  }
+  public unwrap_err_or_else(fn: () => E): E {
+    return this.err.unwrap_or_else(fn);
+  }
+  public map<U, Q>(
+    ok_mapper: (val: T) => U,
+    err_mapper: (err: E) => Q
+  ): Result<U, Q> {
+    if (this.is_ok()) {
+      return Result.Ok<U, Q>(ok_mapper(this.ok_value().unwrap()));
+    } else {
+      return Result.Err<U, Q>(err_mapper(this.err_value().unwrap()));
+    }
+  }
+  public map_ok<U>(fn: (val: T) => U): Result<U, E> {
+    return this.map<U, E>(fn, (err) => err);
+  }
+  public map_err<Q>(fn: (val: E) => Q): Result<T, Q> {
+    return this.map<T, Q>((v) => v, fn);
+  }
+  public transform<To>(fn: (from: typeof this) => To): To {
+    return fn(this);
+  }
+}
