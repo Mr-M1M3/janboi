db6d2e08bc5756e19a9ddb8ff0941bc363465f51
Abir Sheikh <abir.sheikh.4044@gmail.com>
2025-09-13 03:17:10 +0600

    setup prisma with sqlite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  const { data } = $props();
+</script>
+
+<h1>Created topic named {data.name} with id: {data.id}</h1>
