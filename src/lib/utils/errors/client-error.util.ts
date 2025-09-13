15e7c749707e3764bfa06c27ba73968cc623f1d2
Abir Sheikh <abir.sheikh.4044@gmail.com>
2025-09-13 11:58:40 +0600

    improved ClientError and ServerError

--- src/lib/utils/errors/client-error.util.ts
+++ src/lib/utils/errors/client-error.util.ts
@@ -8,18 +8,31 @@ type SerializedClientError<D extends Record<string, unknown>> = {
 export class ClientError<D extends Record<string, unknown>> {
   private message: string;
   private status: number;
+  private details: D;
   constructor(status: number, message: string, details: D) {
     this.status = status;
     this.message = message;
+    this.details = details;
   }
 
-  public serialize<IfWasNotError>(
-    details: D
-  ): Result<IfWasNotError, SerializedClientError<D>> {
+  public to_result<IfWasNotError>(): Result<
+    IfWasNotError,
+    SerializedClientError<D>
+  > {
     return Result.Err<IfWasNotError, SerializedClientError<D>>({
       status: this.status,
       message: this.message,
-      details,
+      details: this.details,
+    });
+  }
+  public serilaize(): SerializedClientError<D> {
+    return this.to_result().transform((from) => {
+      const unwrapped_error = from.unwrap_err();
+      return {
+        status: unwrapped_error.status,
+        message: unwrapped_error.message,
+        details: unwrapped_error.details,
+      };
     });
   }
 }
--- src/lib/utils/errors/server-error.util.ts
+++ src/lib/utils/errors/server-error.util.ts
@@ -10,20 +10,33 @@ export class ServerError<D extends Record<string, unknown>> {
   private id: string;
   private message: string;
   private status: number;
+  private details: D;
   constructor(status: number, message: string, details: D) {
     this.status = status;
     this.id = crypto.randomUUID();
     this.message = message;
+    this.details = details;
   }
-
-  public serialize<IfWasNotError>(
-    details: D
-  ): Result<IfWasNotError, SerializedServerError<D>> {
+  public to_result<IfWasNotError>(): Result<
+    IfWasNotError,
+    SerializedServerError<D>
+  > {
     return Result.Err<IfWasNotError, SerializedServerError<D>>({
       id: this.id,
       status: this.status,
       message: this.message,
-      details,
+      details: this.details,
+    });
+  }
+  public serilaize(): SerializedServerError<D> {
+    return this.to_result().transform((from) => {
+      const unwrapped_error = from.unwrap_err();
+      return {
+        id: unwrapped_error.id,
+        status: unwrapped_error.status,
+        message: unwrapped_error.message,
+        details: unwrapped_error.details,
+      };
     });
   }
 }
