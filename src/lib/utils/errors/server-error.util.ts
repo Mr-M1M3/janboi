dfb376cc33de24d8436eb6e2848f0a0a9e582af4
Abir Sheikh <abir.sheikh.4044@gmail.com>
2025-09-13 11:28:43 +0600

    added different util to handle server error

--- src/lib/utils/errors/server-error.util.ts
+++ src/lib/utils/errors/server-error.util.ts
@@ -1,22 +1,26 @@
 import Result from "../result/result.util";
 
-type SerializedClientError<D extends Record<string, unknown>> = {
+type SerializedServerError<D extends Record<string, unknown>> = {
+  id: string;
   status: number;
   message: string;
   details: D;
 };
-export class ClientError<D extends Record<string, unknown>> {
+export class ServerError<D extends Record<string, unknown>> {
+  private id: string;
   private message: string;
   private status: number;
   constructor(status: number, message: string, details: D) {
     this.status = status;
+    this.id = crypto.randomUUID();
     this.message = message;
   }
 
   public serialize<IfWasNotError>(
     details: D
-  ): Result<IfWasNotError, SerializedClientError<D>> {
-    return Result.Err<IfWasNotError, SerializedClientError<D>>({
+  ): Result<IfWasNotError, SerializedServerError<D>> {
+    return Result.Err<IfWasNotError, SerializedServerError<D>>({
+      id: this.id,
       status: this.status,
       message: this.message,
       details,
